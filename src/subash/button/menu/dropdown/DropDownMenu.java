d4e57ac3b05775d4f343aec84935284a3f41d044
Subash Janarthanan <subashjanarthanan@gmail.com>
2017-06-13 00:09:27 +0530

    [TASK] Create popup menu for right side arrow button control
    Signed-off-by: Subash Janarthanan <subashjanarthanan@gmail.com>

---
+++ src/subash/button/menu/dropdown/DropDownMenu.java
@@ -0,0 +1,128 @@
+package subash.button.menu.dropdown;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * The Class DropDownMenu.
+ * 
+ * @author Subash
+ * 
+ */
+public class DropDownMenu {
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		Display display = new Display();
+		final Shell shell = new Shell(display);
+		shell.setSize(500, 500);
+		shell.setText("SWT Button Arrow Right Drop Down Menu Example");
+		
+		// set layout for the shell
+		shell.setLayout(new RowLayout());
+
+		// creating a composite to add a button
+		final Composite buttonMenuCntrl = new Composite(shell, SWT.BORDER);
+		GridLayoutFactory.fillDefaults().numColumns(2).equalWidth(false).spacing(0, 1).applyTo(buttonMenuCntrl);
+		
+		// Creating CLabel for button Name
+		CLabel lbl = new CLabel(buttonMenuCntrl, SWT.CENTER);
+		lbl.setText("Add Drop Down For Button");
+		
+		// Creating right arrow button to add a right arrow
+		Button rightArrowDropDownBtn = new Button(buttonMenuCntrl, SWT.ARROW | SWT.RIGHT);
+		rightArrowDropDownBtn.setLayoutData(new GridData(GridData.FILL_VERTICAL));
+
+		rightArrowDropDownBtn.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				super.widgetSelected(e);
+				
+				// Create a pop_up main menu for the button
+				Menu menu = new Menu(shell, SWT.POP_UP);
+				
+				// Add menu items that required
+				MenuItem helloMenuItem = new MenuItem(menu, SWT.PUSH);
+				helloMenuItem.setText("Say Helloooooooooooo");
+				helloMenuItem.addSelectionListener(new SelectionListener() {
+					
+					@Override
+					public void widgetSelected(SelectionEvent event) {
+						MessageDialog.openInformation(shell, "HEllO", "Heloooooooooooooooo");	
+					}
+					
+					@Override
+					public void widgetDefaultSelected(SelectionEvent e) {			
+					}
+				});
+				MenuItem hiMenuItem = new MenuItem(menu, SWT.PUSH);
+				hiMenuItem.setText("Say Hiiiiiiiiiiiiiii");
+				hiMenuItem.addSelectionListener(new SelectionListener() {
+					
+					@Override
+					public void widgetSelected(SelectionEvent event) {
+						MessageDialog.openInformation(shell, "HI", "Hiiiiiiiiiiiiiiiiiii");	
+					}
+					
+					@Override
+					public void widgetDefaultSelected(SelectionEvent e) {			
+					}
+				});
+				MenuItem byeMenuItem = new MenuItem(menu, SWT.PUSH);
+				byeMenuItem.setText("Say Byeeeeeeeeeeeeee");
+				byeMenuItem.addSelectionListener(new SelectionListener() {
+					
+					@Override
+					public void widgetSelected(SelectionEvent event) {
+						MessageDialog.openInformation(shell, "BYE", "Byeeeeeeeeeeeeeeeeeee");	
+					}
+					
+					@Override
+					public void widgetDefaultSelected(SelectionEvent event) {			
+					}
+				});
+				MenuItem welcomeMenuItem = new MenuItem(menu, SWT.PUSH);
+				welcomeMenuItem.setText("Say Welcomeeeeeee");
+				welcomeMenuItem.addSelectionListener(new SelectionListener() {
+					
+					@Override
+					public void widgetSelected(SelectionEvent event) {
+						MessageDialog.openInformation(shell, "WELCOME", "Welcomeeeeeeeeeeee");	
+					}
+					
+					@Override
+					public void widgetDefaultSelected(SelectionEvent event) {			
+					}
+				});
+				menu.setVisible(true);
+			}
+		});
+
+		shell.open();
+		
+		// Disposing shell at the end
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
+	}
+
+}
